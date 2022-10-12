47c6a8aec50e72d464c4ca92f2b5ad036a9dabb2
Lukman Abdulkarim <109611523+lukman155@users.noreply.github.com>
2022-10-12 08:22:07 +0100

    Edit: Finish Styling for mobile menu

--- script.js
+++ script.js
@@ -2,12 +2,16 @@ const hamburger = document.querySelector('.hamburger');
 const navi = document.querySelector('.navi');
 const disappear = document.querySelector('.disappear');
 const headerDisappear = document.querySelector('.header-text');
+const text = document.querySelector('.color-text');
+
 hamburger.addEventListener('click', mobileMenu);
+text.addEventListener('click', mobileMenu);
 
 function mobileMenu() {
     headerDisappear.classList.toggle('active')
     disappear.classList.toggle('active');
     hamburger.classList.toggle('active');
     navi.classList.toggle('active');
+    text.classList.toggle('active');
 }
 
